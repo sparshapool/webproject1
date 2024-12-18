0556b02177e7bf6356793f5247bb45386a6e62e1
Sparsha Poolakkal <140339849+sparshapool@users.noreply.github.com>
2024-12-18 13:04:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Theme Toggle
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    themeToggle.textContent = document.body.classList.contains('dark-theme') ? '☀️' : '🌙';
+});
+
